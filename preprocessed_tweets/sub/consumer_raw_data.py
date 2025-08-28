090e92c2c2c93825072710bc7edec93f0de41bed
shuki <shukiheftler@gmail.com>
2025-08-28 16:46:58 +0300

    17:00

--- preprocessed_tweets/sub/consumer_raw_data.py
+++ preprocessed_tweets/sub/consumer_raw_data.py
@@ -1,27 +1,16 @@
 from kafka import KafkaConsumer
 import json
-
-class Subscriber:
-    def __init__(self,topic):
+import os
+class Consumer:
+    def __init__(self, topic1,topic2):
+        kafka_broker = os.getenv("KAFKA_BROKER", "127.0.0.1:9092")
         self.consumer = KafkaConsumer(
-            topic,
-            bootstrap_servers=['kafka:9092'],
-            auto_offset_reset='earliest',
-            enable_auto_commit=True,
-            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+            topic1,topic2,
+            group_id= "preprocess",
+            value_deserializer=lambda m: json.loads(m.decode('utf-8')),
+            bootstrap_servers=[kafka_broker],
+            auto_offset_reset='earliest'
         )
-
-    def consume(self):
-        return self.print_messages()
-
-    def print_messages(self):
-        for message in self.consumer:
-            print(f"{message.topic}:{message.partition}:{message.offset}: "
-                f"key={message.key} value={message.value}")
-
-
-    def __iter__(self):
-        return self.consumer
 
 
 
